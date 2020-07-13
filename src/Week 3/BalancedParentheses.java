80466adad161200de012ad09fc211cdc3fbfa008
chloelau <cwl119@ic.ac.uk>
2020-07-13 13:25:11 +0100

    Initialise Week 3 Skeleton

---
+++ src/Week 3/BalancedParentheses.java
@@ -0,0 +1,11 @@
+// In an interview you can generally omit the import statements,
+// however if you decide to use some library (like Stack in this example)
+// it's a good idea to mention it to the Interviewer and ask if it's allowed.
+import java.util.Stack;
+import org.junit.jupiter.api.Test;
+
+public class BalancedParentheses {
+  public static int longestBalanced(String str) {
+    return 0;
+  }
+}
