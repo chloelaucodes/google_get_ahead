34f400633c4921d0723e87190250d26900a0c1cc
chloelau <cwl119@ic.ac.uk>
2020-07-17 21:44:11 +0100

    Removed Google Style Guide Illegal .* Imports

--- src/Week 2/FlattenedIteratorTest.java
+++ src/Week 2/FlattenedIteratorTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FlattenedIteratorTest {
   @Test
